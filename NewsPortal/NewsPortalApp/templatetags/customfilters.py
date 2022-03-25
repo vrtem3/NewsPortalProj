52d79d32cbd9b2124610bf80a8a85a6005e5b90b
Artem Gorbunov <artem@gorbu.nov.ru>
2022-03-25 10:23:06 +0500

    master

--- NewsPortal/NewsPortalApp/templatetags/customfilters.py
+++ NewsPortal/NewsPortalApp/templatetags/customfilters.py
@@ -3,11 +3,11 @@ from django import template
 register = template.Library()  # если мы не зарегистрируем наши фильтры, то Django никогда не узнает, где именно их искать и фильтры потеряются
 
 
-#replace_values = {'lol': 'l**', 'bot': 'b**'}
+@register.filter(name='censor')
+def censor(text):
 
-#@register.filter(name='censor')
-#def censor(text, replace_values):
-#    for i, j in replace_values.items():
-#        text = text.replace(i, j)
-#    return text
+    replace_values = {'lol': 'l**', 'bot': 'b**'}
 
+    for i, j in replace_values.items():
+        text = text.replace(i, j)
+    return text
